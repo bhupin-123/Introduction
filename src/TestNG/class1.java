45f0c075e706cb2393ee746788b19284f5c2078c
bhupinder <bhupinder.singh2021@gmail.com>
2019-07-17 23:15:11 +0530

    merge conflicts try

--- src/TestNG/class1.java
+++ src/TestNG/class1.java
@@ -12,7 +12,7 @@ public class class1 {
 		System.out.println("PersonLoanLogin2_Original push");
 		System.out.println("PersonLoanLogin2_GG second push");
 		System.out.println("PersonLoanLogin2_develop branch_1_original");
-		System.out.println("PersonLoanLogin2_develop branch_1_GG");
+		System.out.println("PersonLoanLogin2_develop branch_1_G");
 	}
 	
 	@Test
