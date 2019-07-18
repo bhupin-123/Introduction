135d0c345a927adf9cfef7240680b36bac49b3b2
bhupinder <bhupinder.singh2021@gmail.com>
2019-07-18 23:15:53 +0530

    ff

--- src/TestNG/class1.java
+++ src/TestNG/class1.java
@@ -12,7 +12,6 @@ public class class1 {
 		System.out.println("PersonLoanLogin1");
 		System.out.println("PersonLoanLogin2");
 		System.out.println("PersonLoanLogin3");
-		System.out.println("PersonLoanLogin4");
 		System.out.println("PersonLoanLogin5");
 		System.out.println("PersonLoanLogin6");
 		System.out.println("PersonLoanLogin7");
